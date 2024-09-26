e9f0f806f74d7bc4e8c9a238ce58394ce7889174
Core-Team-BGSW <mohit.somvanshi@bosch.com>
2024-09-26 22:27:37 +0530

    final commit

---
+++ src/main/java/com/mohits/cab_book_driver/constant/AppConstant.java
@@ -0,0 +1,5 @@
+package com.mohits.cab_book_driver.constant;
+
+public class AppConstant {
+    public static final String CAB_LOCATION = "cab-location";
+}
---
+++ src/test/java/com/mohits/cab_book_driver/CabBookDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohits.cab_book_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
