d057a65b4fea4a0c4d1b535e38b77f12c691cfad
Core-Team-BGSW <mohit.somvanshi@bosch.com>
2024-09-27 10:17:55 +0530

    first commit

---
+++ src/test/java/com/mohits/cab_book_user/CabBookUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohits.cab_book_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
